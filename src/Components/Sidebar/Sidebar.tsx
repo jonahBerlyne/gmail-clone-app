1fbc28c7fc8323a2b18d958ce265ffb96757c69c
jonahBerlyne <jonshaber@gmail.com>
2022-05-14 13:54:24 -0400

    added sidebar options to sidebar

--- src/Components/Sidebar/Sidebar.tsx
+++ src/Components/Sidebar/Sidebar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import "../../Styles/Sidebar.css";
-import { Button } from "@mui/material";
-import { Add, Inbox } from "@mui/icons-material";
+import { Button, IconButton } from "@mui/material";
+import { Add, Inbox, Star, AccessTime, LabelImportant, NearMe, Note, ExpandMore, Person, Duo, Phone } from "@mui/icons-material";
 import SidebarOption from "./SidebarOption";
 
 export default function Sidebar() {
@@ -18,7 +18,53 @@ export default function Sidebar() {
         icon={<Inbox />}
         title="Inbox"
         number={54}
+        selected={true}
       />
+      <SidebarOption 
+        icon={<Star />}
+        title="Starred"
+        number={23}
+      />
+      <SidebarOption 
+        icon={<AccessTime />}
+        title="Snoozed"
+        number={82}
+      />
+      <SidebarOption 
+        icon={<LabelImportant />}
+        title="Important"
+        number={44}
+      />
+      <SidebarOption 
+        icon={<NearMe />}
+        title="Sent"
+        number={5}
+      />
+      <SidebarOption 
+        icon={<Note />}
+        title="Drafts"
+        number={2}
+      />
+      <SidebarOption 
+        icon={<ExpandMore />}
+        title="More"
+        number={17}
+      />
+
+      <div className="sidebar-footer">
+        <div className="sidebar-footer-icons">
+          <IconButton>
+            <Person />
+          </IconButton>
+          <IconButton>
+            <Duo />
+          </IconButton>
+          <IconButton>
+            <Phone />
+          </IconButton>
+        </div>
+      </div>
+
     </div>
   );
 }
--- src/Components/Sidebar/SidebarOption.tsx
+++ src/Components/Sidebar/SidebarOption.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import "../../Styles/SidebarOption.css";
+import "../../Styles/Sidebar.css";
 
 interface Option {
   icon: any;
   title: string;
   number: number;
+  selected?: boolean;
 };
 
-export default function SidebarOption({ icon, title, number }: Option) {
+export default function SidebarOption({ icon, title, number, selected }: Option) {
   return (
-    <div>SidebarOption</div>
+    <div className={`sidebar-option ${selected && "sidebar-option-active"}`}>
+      {icon}
+      <h3>{title}</h3>
+      <p>{number}</p>
+    </div>
   );
 }
