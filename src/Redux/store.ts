38f4ee76c9cf82470ffae3b30c6265965ebd0cd5
jonahBerlyne <jonshaber@gmail.com>
2022-05-13 21:18:37 -0400

    added slices folder to redux

--- src/Redux/store.ts
+++ src/Redux/store.ts
@@ -1,5 +1,5 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from './counterSlice';
+import counterReducer from './Slices/userSlice';
 
 export const store = configureStore({
   reducer: {
