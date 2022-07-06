09bdafbca6fd27a783f72c2faa044d04551310c9
jonahBerlyne <jonshaber@gmail.com>
2022-07-06 16:45:59 -0400

    added responsiveness to email row

--- src/Components/EmailRow.tsx
+++ src/Components/EmailRow.tsx
@@ -6,7 +6,7 @@ import { useNavigate } from "react-router-dom";
 import { useAppDispatch } from '../Redux/hooks';
 import { selectMail } from "../Redux/Slices/mailSlice";
 import { deleteDoc, doc } from 'firebase/firestore';
-import fireDB, { auth } from '../firebaseConfig';
+import fireDB from '../firebaseConfig';
 import { getAuth } from 'firebase/auth';
 
 interface Row {
@@ -47,26 +47,34 @@ export default function EmailRow({ from, subject, msg, time, id }: Row) {
     <div key={id} className='email-row'>
 
       <div className="email-row-options">
-        <Checkbox />
-        <IconButton>
-          <StarBorderOutlined />
-        </IconButton>
-        <IconButton>
-          <LabelImportantOutlined />
-        </IconButton>
-        <IconButton data-testid={`deleteEmailBtn${id}`}  onClick={deleteEmail}>
-          <Delete />
-        </IconButton>
+        <div className="email-row-options-left">
+          <Checkbox />
+          <IconButton data-testid={`deleteEmailBtn${id}`}  onClick={deleteEmail}>
+            <Delete />
+          </IconButton>
+        </div>
+        <div className="email-row-options-right">
+          <IconButton>
+            <StarBorderOutlined />
+          </IconButton>
+          <IconButton>
+            <LabelImportantOutlined />
+          </IconButton>
+        </div>
       </div>
       <div data-testid={`openMail${id}`} className="email-row-message" onClick={openMail}>
-          <h3 data-testid={`emailFrom${id}`} className="email-row-from">
-            {from}
-          </h3>
-          <h4 data-testid={`subj${id}`}>{subject}{""}</h4>
-          <span data-testid={`msg${id}`} className="email-row-description">{msg}</span>
-          <p data-testid={`time${id}`} className="email-row-time">
-            {time}
-          </p>
+          <div className="email-row-message-left">
+            <h3 data-testid={`emailFrom${id}`} className="email-row-from">
+              {from}
+            </h3>
+            <h4 data-testid={`subj${id}`}>{subject}{""}</h4>
+            <span data-testid={`msg${id}`} className="email-row-description">{msg}</span>
+          </div>
+          <div className="email-row-message-right">
+            <p data-testid={`time${id}`} className="email-row-time">
+              {time}
+            </p>
+          </div>
       </div>
 
     </div>
